67baa81ae0f381be32f1a4eb6a1e45c046d0ceb3
Dmitry Bubyakin <dimabubyakin97@gmail.com>
2024-08-09 09:35:34 +0300

    fix aliases

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import { cn } from "../lib/utils";
+import { cn } from "@3ab-users/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
